ffab177f0bf9a5d8e82b6dbc0b18d007f58f4c70
FabioLuz87 <fabioddh@hotmail.com>
2022-10-17 13:24:17 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, {Request, Response} from 'express';
 import routes from './routes';
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
 routes(app);
+
 
 app.listen(8080, () => console.log('server start'));
