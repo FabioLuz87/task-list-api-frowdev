4842b266a381462e65f090bca8955ddd0595481f
FabioLuz87 <fabioddh@hotmail.com>
2023-01-05 22:29:52 -0300

    feat: implements migrations

---
+++ src/main/database/migrations/1672965127829-CreateTableTasks.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm"
+
+export class CreateTableTasks1672965127829 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "tasks",
+                columns: [
+                    { name: "id", type: "uuid", isPrimary: true, isNullable: false },
+                    { name: "description", type: "varchar", length: "50", isNullable: false },
+                    { name: "detail", type: "varchar", length: "255", isNullable: false },
+                    { name: "is_it_archived", type: "boolean", isNullable: false, default: false },
+                    {
+                    name: "created_at",
+                    type: "timestamp",
+                    isNullable: false,
+                    default: "now()",
+                    },
+                    {
+                    name: "updated_at",
+                    type: "timestamp",
+                    isNullable: false,
+                    default:  "now()",
+                    },
+                ],
+                foreignKeys: [
+                    new TableForeignKey({
+                    name: "fk_user_id",
+                    columnNames: ["user_id"],
+                    referencedTableName: "users",
+                    referencedColumnNames: ["id"],
+                    }),
+                ]
+
+            })
+        
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
