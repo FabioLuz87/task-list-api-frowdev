836661c6ecb7e49d8328c7be2b6e20b53759d10a
FabioLuz87 <fabioddh@hotmail.com>
2023-01-07 10:38:58 -0300

    feat: includes cache methods in tasks usecases

--- src/app/features/task/usecases/edit-task.usecase.ts
+++ src/app/features/task/usecases/edit-task.usecase.ts
@@ -1,23 +1,26 @@
 import { TaskRepository } from "./repositories/task.repository";
 import { Task } from "../../../models/task.model";
+import { CacheRepository } from "../../../shared/database/repositories/cache.respository";
 
 export class EditTaskUsecase{
     private _repository: TaskRepository;
+    private _cacheRepository: CacheRepository;
 
     constructor() {
         this._repository = new TaskRepository();
+        this._cacheRepository = new CacheRepository();
     }
 
     async execute(id: string, description: string, detail: string ): Promise<Task> {
 
         const task = await this._repository.findTaskById(id);
-        if(!task) throw new Error("Mensagem não encontrada");
+        if(!task) throw new Error("Impossível encontrar tarefa");
         
         task.update(description, detail);
         this._repository.update(task);
 
+        await this._cacheRepository.delete(`tasks:${task.userId}`)
+
         return task;
     }
-
-
 }
--- src/app/features/task/usecases/remove-task.usecase.ts
+++ src/app/features/task/usecases/remove-task.usecase.ts
@@ -1,14 +1,21 @@
+import { CacheRepository } from "../../../shared/database/repositories/cache.respository";
 import { TaskRepository } from "./repositories/task.repository";
 
 export class RemoveTaskUsecase{
     private _repository: TaskRepository;
+    private _chacheRepository: CacheRepository;
 
     constructor() {
         this._repository = new TaskRepository();
+        this._chacheRepository = new CacheRepository();
     }
 
     async execute(id: string): Promise<void> {
 
+        const task = await this._repository.findTaskById(id)
+        if(!task) throw new Error("Impossível encontrar tarefa");
+
         await this._repository.delete(id);
+        await this._chacheRepository.delete(`tasks:${task.userId}`);
     }
 }
