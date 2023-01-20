4a9599df41abb85213d5a996fdebf8646bdfd99f
FabioLuz87 <fabioddh@hotmail.com>
2023-01-19 23:34:35 -0300

    teste: cria teste unitário de criar usuário

---
+++ tests/app/features/user/create-user.usecase.spec.ts
@@ -0,0 +1,22 @@
+import { UserRepository } from '../../../../src/app/features/user/repositories/user.repository';
+import { CreateUserUsecase } from '../../../../src/app/features/user/usecases/create-user.usecase'
+
+describe('Create user Usecase', () => {
+    test('Deve salvar um user', async () => {
+
+        const repository = new UserRepository();
+        const sut = new CreateUserUsecase(repository);
+
+        jest.spyOn(repository, 'verifyUserExistsByEmail').mockResolvedValue(false);
+        jest.spyOn(repository, 'saveUser').mockResolvedValue();
+
+        const result = await sut.execute({
+           name: 'any_name',
+           email: 'any_email',
+           pass: 'any_pass',
+        });
+
+        expect(result.name).toBe('any_name');
+
+    });
+});
