be01ee6419d48159221c026edb4bbad620b17c3f
FabioLuz87 <fabioddh@hotmail.com>
2023-01-19 23:34:12 -0300

    test: cria teste unitário de login

---
+++ tests/app/features/authentication/login-user.usecase.spec.ts
@@ -0,0 +1,24 @@
+import { AuthRepository } from '../../../../src/app/features/authentication/repositories/auth.repository';
+import { LoginUser } from '../../../../src/app/features/authentication/usecases/login-user.usecase';
+import { User } from '../../../../src/app/models/user.model';
+
+describe('Login user Usecase', () => {
+    test('Deve realizar login de um user', async () => {
+
+        const repository = new AuthRepository();
+        const sut = new LoginUser(repository);
+        const user = new User('any_name', 'any_email', 'any_pass');
+
+        jest.spyOn(repository, 'findUserByEmail').mockResolvedValue(user);
+      
+        const result = await sut.execute({
+           email: 'any_email',
+           pass: 'any_pass',
+        });
+
+        expect(result.id).toBeTruthy();
+        expect(result.email).toBe('any_email');
+        expect(result.pass).toBe('any_pass');
+
+    });
+});
